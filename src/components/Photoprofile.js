a9b3b9616c954ffdf28437998e83d99dc568ad4e
vatsalshah041 <shahvatsal2003@gmail.com>
2023-03-12 02:37:31 +0530

    Profile Page

--- src/components/Photoprofile.js
+++ src/components/Photoprofile.js
@@ -1,22 +1,80 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Grid, Typography } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 import Avatar from '@mui/material/Avatar';
 import '../styles/photoprofile.css'
+
+import { useState, useEffect } from 'react';
 import { Container } from '@mui/system';
 import Tab_photo from './Tab_photo';
+import axios from 'axios';
 
 
 export default function Photoprofile() {
+    const [data,setData]=useState();
+    useEffect(() => {
+
+        var bear="Bearer "+localStorage.getItem("access");
+        var config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: '//127.0.0.1:8000/account/profile/',
+            headers: {
+                'Authorization':bear
+            }
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                setData(response.data)
+                console.log(data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+
+    }, []);
+
+    const edit=()=>{
+
+    
+    
+    }
+
+
     return (
         <>
             <Navbar></Navbar>
-            <Grid container sx={{ display: 'flex' }} className="profile">
+            {data?(<><Grid container sx={{ display: 'flex' }} className="profile">
                 <Grid item md={6} sx={{ display: 'flex', justifyContent: 'center' }}>
                     <Avatar
                         className='image1'
-                        alt="Remy Sharp"
-                        src="/static/images/avatar/1.jpg"
+                        alt="pfp"
+                        src=""
+                        sx={{ width: 300, height: 300 }}
+                    />
+                </Grid>
+                <Grid item md={6} sx={{ justifyContent: 'center' }}>
+                    <Typography variant='h3'>
+                        {data.username}
+                    </Typography>
+                    <Typography variant="h6">Bio is here Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam aliquid accusantium a labore facilis excepturi blanditiis laborum facere? Eum recusandae repellendus dolores est velit quo dignissimos quos accusantium quidem.</Typography>
+                </Grid>
+                <Grid item md={12}>
+                    <Button variant='outlined' onClick={edit}>
+                        Edit
+                    </Button>
+                </Grid>
+            </Grid>
+            <hr />
+            <Tab_photo></Tab_photo></>):(<><Grid container sx={{ display: 'flex' }} className="profile">
+                <Grid item md={6} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <Avatar
+                        className='image1'
+                        alt="pfp"
+                        src=""
                         sx={{ width: 300, height: 300 }}
                     />
                 </Grid>
@@ -26,10 +84,12 @@ export default function Photoprofile() {
                     </Typography>
                     <Typography variant="h6">Bio is here Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam aliquid accusantium a labore facilis excepturi blanditiis laborum facere? Eum recusandae repellendus dolores est velit quo dignissimos quos accusantium quidem.</Typography>
                 </Grid>
-                <Grid item={12}></Grid>
+                <Grid item md={12}>
+                </Grid>
             </Grid>
             <hr />
-            <Tab_photo></Tab_photo>
+            <Tab_photo></Tab_photo></>)}
+            
 
         </>
     )
