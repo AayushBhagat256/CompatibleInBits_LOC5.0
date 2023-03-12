cb57fe821189fd20c56da20d98982b9de5cd4511
vatsalshah041 <shahvatsal2003@gmail.com>
2023-03-12 09:35:09 +0530

    integration

--- src/components/CustomerPanel.js
+++ src/components/CustomerPanel.js
@@ -15,91 +15,131 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import Button from '@mui/material/Button'
 import Navbar from './Navbar';
+import { useState } from 'react';
+import { display } from '@mui/system';
+import axios from 'axios';
+import Photoprofile from './Photoprofile';
+import { Link } from 'react-router-dom';
 
 function CustomerPanel() {
-    document.addEventListener('contextmenu', event => event.preventDefault());
-    const theme = useTheme();
-    // const [val,setVal] = useState('')
-    // const names = ['James', 'John', 'Paul', 'Ringo', 'George'];
+  const [d,setD]=useState();
+  const [domain, setDomain] = useState();
+  document.addEventListener('contextmenu', event => event.preventDefault());
+  const theme = useTheme();
+  // const [val,setVal] = useState('')
+  // const names = ['James', 'John', 'Paul', 'Ringo', 'George'];
+  var bear="Bearer "+localStorage.getItem("access")
+  const check1 = () => {
+    let domain1 = document.getElementById('optlist').value
+    console.log(domain1)
+    setDomain(domain1);
+    display();
+  }
+  const display = () => {
+var data = JSON.stringify({
+  "name": "portrait"
+});
 
-    const check1 = () => {
-        let domain = document.getElementById('optlist').value
-        console.log(domain)
-    }
+var config = {
+  method: 'get',
+maxBodyLength: Infinity,
+  url: '//127.0.0.1:8000/account/domainspecific/food/',
+  headers: { 
+    'Authorization': bear, 
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
 
-    return (
-        <>
-            <Navbar/>
-            <div className='container-class'>
-                <div className="heading">
-                    {/* <h1>This is cp</h1> */}
-                </div>
+axios(config)
+.then(function (response) {
+  console.log((response.data));
+  setD(response.data)
+})
+.catch(function (error) {
+  console.log(error);
+});
 
-                {/* <input type="text" id="myInput" onChange={e=>setVal(e.target.value)} value={val} placeholder="Search for names.."/>
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className='container-class'>
+        <div className="heading">
+          {/* <h1>This is cp</h1> */}
+        </div>
+
+        {/* <input type="text" id="myInput" onChange={e=>setVal(e.target.value)} value={val} placeholder="Search for names.."/>
       {names.filter(name => name.includes(val)).map(filteredName => (
         <li>
           {filteredName}
         </li>
       ))} */}
-                <div className="optionsList">
-                    <p>Select a domain as per your requirement</p><select name="" id="optlist">
-                        <option value="dom1">dom1</option>
-                        <option value="dom2">dom2</option>
-                        <option value="dom3">dom3</option>
-                        <option value="dom4">dom4</option>
-                        <option value="dom5">dom5</option>
-                    </select>
+        <div className="optionsList">
+          <p>Select a domain as per your requirement</p><select name="" id="optlist">
+            <option value="food">Food</option>
+            <option value="portrait">Portrait</option>
+            <option value="coorporate_events">Coorporate events</option>
+            <option value="coorporate_headshots">Coorporate Headshots</option>
+            <option value="private_events">Private event</option>
+          </select>
 
-                </div>
-                <button id='apply' onClick={check1}>Apply</button>
+        </div>
+        <button id='apply' onClick={check1}>Apply</button>
 
-            </div>
-            <div className="container-cld">
-            <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto',height:'30vh',width:'25vw' }}>
-          <Typography component="div" variant="h5">
-            <div className="personaldata">
-                <div className="avatar">
+      </div>
+      
+      {d?(<div>
+
+        
+      <div className="container-cld">
+        <Card sx={{ display: 'flex' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CardContent sx={{ flex: '1 0 auto', height: '30vh', width: '25vw' }}>
+              <Typography component="div" variant="h5">
+                <div className="personaldata">
+                  <div className="avatar">
                     {/* <Avatar>P</Avatar> */}
+                  </div>
+                  <div className="name">
+                    <h4>{d[0].username}</h4>
+                  </div>
                 </div>
-                <div className="name">
-                    <h4>Name here</h4>
-                </div>
-            </div>
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            Review here
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime nobis maiores, quisquam aspernatur asperiores culpa deleniti, ab autem adipisci et cum voluptates a molestiae, quos sit repellat? Nihil molestiae fugit accusantium minus. Doloribus, ea.
-          </Typography>
-          
-        </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-        <Button>Explore more</Button>
-          {/* <IconButton aria-label="previous">
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                Review
+                </Typography>
+
+            </CardContent>
+            <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+              <Button variant='outlined'>
+                <Link to='/photoprofile'>Explore</Link>
+              </Button>
+              {/* <IconButton aria-label="previous">
             {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
           </IconButton> */}
-          
-          {/* <IconButton aria-label="play/pause">
+
+              {/* <IconButton aria-label="play/pause">
             <PlayArrowIcon sx={{ height: 38, width: 38 }} />
           </IconButton> */}
-          {/* <IconButton aria-label="next">
+              {/* <IconButton aria-label="next">
             {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
           </IconButton> */}
-        </Box>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 ,marginRight:5}}
-        image={Image}
-        alt="Live from space album cover"
-      />
-    </Card>
-            </div>
+            </Box>
+          </Box>
+          <CardMedia
+            component="img"
+            sx={{ width: 151, marginRight: 5 }}
+            image={Image}
+            alt="Live from space album cover"
+          />
+        </Card>
+      </div>
+      </div>):(<div>ghhh</div>)}
 
-
-        </>
-    )
+    </>
+  )
 }
 
 export default CustomerPanel
